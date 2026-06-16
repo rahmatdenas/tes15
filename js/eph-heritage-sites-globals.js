4a57d1e0d4e976c62894c9bfff6acff15fe0bfa6
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-06-16 10:02:42 +0700

    Efesiensi waktu loading

--- js/eph-heritage-sites-globals.js
+++ js/eph-heritage-sites-globals.js
@@ -101,20 +101,6 @@ const SPARQL_QUERY_3 =
     FILTER NOT EXISTS { ?imageStatement pq:P180 wd:Q192630 }
   }
   
-  # 2. AMBIL GAMBAR LINGKUNGAN SEKITAR (Dibiarkan tanpa SAMPLE agar tampil semua)
-  OPTIONAL {
-    ?site p:P18 ?vicinityStatement .
-    ?vicinityStatement ps:P18 ?vicinityImage .
-    FILTER EXISTS { ?vicinityStatement pq:P3831 wd:Q16189205 }
-  }
-
-  # 3. AMBIL GAMBAR MASA LALU
-  OPTIONAL {
-    ?site p:P18 ?pastImgStmt .
-    ?pastImgStmt ps:P18 ?imgMasaLalu .
-    ?pastImgStmt pq:P180 wd:Q192630 .
-  }
-
   # 4. ARTIKEL WIKIPEDIA
   OPTIONAL {
     ?wikipedia schema:about ?site ;
@@ -123,42 +109,66 @@ const SPARQL_QUERY_3 =
   }
   
   BIND (SUBSTR(STR(?site), 32) AS ?siteQid) .
-} GROUP BY ?siteQid ?vicinityImage`;
-
-// 7. SPARQL_QUERY_4: Mengambil Peristiwa Penting (P793) beserta Detail Waktu
-const SPARQL_QUERY_4 =
-`SELECT ?siteQid ?eventLabel ?pointInTime ?ptPrecision ?startTime ?stPrecision ?endTime ?etPrecision WHERE {
-  <SPARQLVALUESCLAUSE>
-  
-  # Ambil node pernyataan peristiwa penting
-  ?site p:P793 ?eventStatement .
-  
-  # Ambil objek peristiwanya
-  ?eventStatement ps:P793 ?event .
-  
-  # Ambil nama peristiwanya dalam bahasa Indonesia
-  ?event rdfs:label ?eventLabel . 
-  FILTER(LANG(?eventLabel) = "id") .
-  
-  # Ambil kualifikasi waktu beserta PRESISINYA menggunakan node pqv (bukan sekadar pq)
-  OPTIONAL { 
-    ?eventStatement pqv:P585 ?ptNode .
-    ?ptNode wikibase:timeValue ?pointInTime ;
-            wikibase:timePrecision ?ptPrecision .
-  }
-  OPTIONAL { 
-    ?eventStatement pqv:P580 ?stNode .
-    ?stNode wikibase:timeValue ?startTime ;
-            wikibase:timePrecision ?stPrecision .
-  }
-  OPTIONAL { 
-    ?eventStatement pqv:P582 ?etNode .
-    ?etNode wikibase:timeValue ?endTime ;
-            wikibase:timePrecision ?etPrecision .
-  }
-  
-  BIND (SUBSTR(STR(?site), 32) AS ?siteQid) .
 }`;
+
+// 7. SPARQL_QUERY_4: Fungsi khusus mengambil Peristiwa Penting untuk satu ID saat diklik
+function getSparqlQuery4(qid) {
+  return `SELECT ?siteQid ?eventLabel ?pointInTime ?ptPrecision ?startTime ?stPrecision ?endTime ?etPrecision WHERE {
+    VALUES ?site { wd:${qid} }
+    
+    # Ambil node pernyataan peristiwa penting
+    ?site p:P793 ?eventStatement .
+    
+    # Ambil objek peristiwanya
+    ?eventStatement ps:P793 ?event .
+    
+    # Ambil nama peristiwanya dalam bahasa Indonesia
+    ?event rdfs:label ?eventLabel . 
+    FILTER(LANG(?eventLabel) = "id") .
+    
+    # Ambil kualifikasi waktu beserta PRESISINYA menggunakan node pqv (bukan sekadar pq)
+    OPTIONAL { 
+      ?eventStatement pqv:P585 ?ptNode .
+      ?ptNode wikibase:timeValue ?pointInTime ;
+              wikibase:timePrecision ?ptPrecision .
+    }
+    OPTIONAL { 
+      ?eventStatement pqv:P580 ?stNode .
+      ?stNode wikibase:timeValue ?startTime ;
+              wikibase:timePrecision ?stPrecision .
+    }
+    OPTIONAL { 
+      ?eventStatement pqv:P582 ?etNode .
+      ?etNode wikibase:timeValue ?endTime ;
+              wikibase:timePrecision ?etPrecision .
+    }
+    
+    BIND (SUBSTR(STR(?site), 32) AS ?siteQid) .
+  }`;
+
+
+// 8. SPARQL_QUERY_5: Fungsi khusus mengambil arsip gambar untuk satu ID saat diklik
+function getSparqlQuery5(qid) {
+  return `SELECT ?siteQid ?vicinityImage ?pastImage WHERE {
+    VALUES ?site { wd:${qid} }
+    
+    # 1. AMBIL GAMBAR LINGKUNGAN SEKITAR
+    OPTIONAL {
+      ?site p:P18 ?vicinityStatement .
+      ?vicinityStatement ps:P18 ?vicinityImage .
+      FILTER EXISTS { ?vicinityStatement pq:P3831 wd:Q16189205 }
+    }
+
+    # 2. AMBIL GAMBAR MASA LALU (Hilangkan fungsi SAMPLE agar kueri murni asinkronus tanpa GROUP BY)
+    OPTIONAL {
+      ?site p:P18 ?pastImgStmt .
+      ?pastImgStmt ps:P18 ?pastImage .
+      ?pastImgStmt pq:P180 wd:Q192630 .
+    }
+
+    BIND (SUBSTR(STR(?site), 32) AS ?siteQid) .
+  }`;
+}
 
 // 8. ABOUT_SPARQL_QUERY
 const ABOUT_SPARQL_QUERY = ``;
